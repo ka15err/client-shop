1314d5de02c8af7f958e43905f27d45289b0639c
kLot2005 <134000518+kLot2005@users.noreply.github.com>
2024-04-29 16:40:18 +0500

    logo fixset

--- components/modules/Footer/FooterLogo.tsx
+++ components/modules/Footer/FooterLogo.tsx
@@ -6,7 +6,7 @@ const FooterLogo = () => (
   <div className={styles.footer__top__item}>
     <Link href="/dashboard" passHref legacyBehavior>
       <a className={styles.footer__top__item__logo}>
-        <img src="/img/logo-footer.svg" alt="logo" />
+        <img src="/img/blue-fon-logo.svg" alt="logo" />
         <span className={styles.footer__top__item__logo__text}>
           Детали для газовых котлов
         </span>
